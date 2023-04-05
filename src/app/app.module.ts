1c5072bc2734871bb98084569edaa62caa800ddd
Gazizov Ruslan <almetfly@gmail.com>
2023-04-05 09:46:16 +0300

    feat: base demo functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KonvaManipulatorComponent } from './components/konva-manipulator/konva-manipulator.component';
+
+// Import KonvaModule
+import { KonvaModule } from 'ng2-konva';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, KonvaManipulatorComponent],
+  imports: [BrowserModule, AppRoutingModule, KonvaModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
